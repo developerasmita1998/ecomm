0c312227b3320d039e8365763d09ccba56a3368f
Asmita Kumari <asmitababhanlai@gmail.com>
2024-02-17 14:03:47 +0530

    Login page done

---
+++ src/screens/Register.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import './../App.css';
+
+function Register() {
+  return (
+    <div className="container">
+        
+        
+        yaha Register banaogi
+
+        ban jayega naa ?
+       hm 
+      
+    </div>
+  );
+}
+
+export default Register;
